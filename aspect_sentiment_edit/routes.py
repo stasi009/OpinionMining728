a0857b4f9e59259770a3b0f4b362c0f8ddf09968
stasi009 <stasi009@163.com>
2016-08-15 16:23:22 +0800

    put aspect into a dropdown list

--- aspect_sentiment_edit/routes.py
+++ aspect_sentiment_edit/routes.py
@@ -3,6 +3,21 @@ import json
 from flask import Flask,url_for,request,render_template,redirect
 from app import app
 from review import Review,ReviewsDal
+import common
+
+AspectOptions = [   common.AspectOverall,\
+                    common.AspectClean,\
+                    common.AspectLocation,\
+                    common.AspectRoom,\
+                    common.AspectService,\
+                    common.AspectValue,\
+                    common.AsepctNothing,\
+                    common.AspectBusiness,\
+                    common.AspectUnknown]
+SentimentOptions = [    common.SentimentPositive,\
+                        common.SentimentNegative,\
+                        common.SentimentNeutral,\
+                        common.SentimentUnknown]
 
 @app.route('/')
 def home():
@@ -11,4 +26,4 @@ def home():
 @app.route('/randomreview')
 def next_random_review():
     review = app.mongoproxy.next_random_review()
-    return render_template("review.html",review = review)
+    return render_template("review.html",review = review,aspect_options = AspectOptions,sentiment_options = SentimentOptions)
