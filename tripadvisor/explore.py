01c12363243058e78991a90f646236cfee6e2fe1
stasi009 <stasi009@163.com>
2016-08-20 13:44:28 +0800

    build most common words based on aspect

--- tripadvisor/explore.py
+++ tripadvisor/explore.py
@@ -1,44 +1,82 @@
 
-import random
-random.seed(689)
-
 import os,sys
-sys.path.append(os.path.abspath(".."))
+parentpath = os.path.abspath("..")
+if parentpath not in sys.path:
+    sys.path.append(parentpath)
 
+import random
+import csv
 import nltk
-from nltk.corpus import stopwords
-import logging
-import glob
+from nltk.classify import NaiveBayesClassifier
+from nltk.classify.util import accuracy
 
-from entities import Hotel
-import sentence
+def get_samples_stream(filename):
+    with open(filename,"rt") as inf:
+        reader = csv.reader(inf)
+        for row in reader:
+            yield (row[0],row[1].split() )
 
-def review_stream(n_hotels):
-    json_files = glob.glob( "data/*.json" )
-    selected_hotels = random.sample(json_files,n_hotels)
+def statistics_by_aspect():
+    filename = "aspects_train.csv"
+    words_dist = nltk.ConditionalFreqDist()
+    sample_sizes = nltk.FreqDist()
 
-    for hfile in selected_hotels:
-        hotel = Hotel(hfile)
-        for review in hotel.reviews:
-            review_text = review.title + ". " + review.content if len(review.title) >0 else review.content
-            yield review_text
+    samples_stream = get_samples_stream(filename)
+    for aspect,words in samples_stream:
+        sample_sizes[aspect] += 1
+        for word in words:
+            words_dist[aspect][word] += 1
 
-def word_stream(review_stream):
-    stop_words = set(stopwords.words("english"))
-    for index,review in enumerate(review_stream):
-        # treat the whole review as one long sentence
-        sent = sentence.Sentence(review,stop_words)
+    for category,dist in words_dist.iteritems():
+        print "\n------- Category: {}".format(category)
+        print dist.most_common(20)
 
-        for word in sent.words:
-            yield word
+    total_samples = sample_sizes.N()
+    print "\ntotally {} samples".format(total_samples)
+    for aspect, count in sample_sizes.iteritems():
+        print "aspect[{}] has {} samples, {:.2f}%".format(aspect,count, count*100.0/total_samples)
 
-        print "{}-th review processed".format(index+1)
+def binary_bow_feature(samples):
+    # samples is a list, where each cell is a tuple
+    # tuple[0] is the aspect
+    # tuple[1] is the words
+    # return result is also a list where each cell is a tuple
+    # tuple[0] is the features represented by a dictionary
+    # tuple[1] is the aspect
+    return ( ( {}.fromkeys(words,True),  aspect) for aspect, words in samples  )
+
+def split_samples(all_samples,test_sample_ratio):
+    random.shuffle(all_samples)
+    test_size = int(len(all_samples) * test_sample_ratio)
+    test_samples,train_samples = all_samples[:test_size],all_samples[test_size:]
+    return train_samples,test_samples
+
+def print_errors(classifier,samples):
+    for features,real_label in samples:
+        result_label = classifier.classify(features)
+        if result_label != real_label:
+            print "\n ############# {}".format(features)
+            print "real_label: {}, my result: {}".format(real_label,result_label)
+
+def naivebayes_classify(filename):
+    raw_sample_stream = get_samples_stream(filename)
+    all_samples = list( binary_bow_feature(raw_sample_stream) )
+
+    test_sample_ratio = 0.25
+    train_samples,test_samples = split_samples(all_samples,test_sample_ratio)
+    print "training set has {} samples, test set has {} samples".format(len(train_samples),len(test_samples))
+
+    classifier = NaiveBayesClassifier.train(train_samples)
+    print "training completes"
+
+    print "training accuracy: {}".format(accuracy(classifier,train_samples))
+    print "test accuracy: {}".format(accuracy(classifier,test_samples))
+    classifier.show_most_informative_features(n=10)
+
+    print_errors(classifier,all_samples)
+
+    return classifier
 
 if __name__ == "__main__":
-    N_HOTELS = 300
-    stream_of_word = word_stream(review_stream(N_HOTELS))
-    fdist = nltk.FreqDist(stream_of_word)
-
-    with open("most_common_words.csv","wt") as outf:
-        for (word,count) in fdist.most_common(100):
-            outf.write("{},{}\n".format(word,count))
+    # statistics_by_aspect()
+    classifier = naivebayes_classify("aspects_train.csv")
